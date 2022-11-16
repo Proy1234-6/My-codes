0cdcb2e2f0f253719d9cbb138eba52bfdc278d27
Proy1234-6 <111143294+Proy1234-6@users.noreply.github.com>
2022-11-16 20:58:08 +0530

    Add files via upload

---
+++ intrgration(trapizoidal).py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def trap(xi,xf,n):
+    
+    yy=[]
+    h=(xf-xi)/(n-1)
+
+    for i in range(n):
+        x=xi+(i*h)
+        #y=x**2
+        y=np.sin(x)
+        yy.append(y)
+
+    s=0
+    for i in range(1,n-1):
+        s+= yy[i]
+    s=2*s
+    s=s+yy[0]+yy[n-1]
+    
+    return (h/2)*s
+
+xi=-(np.pi)
+xf=np.pi
+n=50
+for i in range(2,n+1):
+    print("Taking ",i,"Points the value is-",trap(xi,xf,n))
